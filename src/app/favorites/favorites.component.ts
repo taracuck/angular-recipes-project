0a22cde61f7d33bc90c88b1431896059ab18af3a
pskuthe@gmail.com <pskuthe@gmail.com>
2021-02-21 15:43:29 -0500

    favorite page

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
 })
 export class FavoritesComponent implements OnInit {
-
-  constructor() { }
+  favorites: any[] = [];
+  filteredRecipes: any[] = [];
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.getFavorites();
   }
-
+  getFavorites = (): void => {
+    this.favorites = this.recipeService.getFavorites();
+  };
+  toggleFavorite = (favorite: any): void => {
+    console.log(favorite);
+    if (this.recipeService.checkIfFavorite(favorite)) {
+      this.recipeService.removeFavorite(favorite);
+    } else {
+      this.recipeService.addFavorite(favorite);
+    }
+    this.getFavorites();
+  };
+  getAndSetRecipes = (formObject: any): void => {
+    this.filteredRecipes = this.recipeService
+      .getRecipes(formObject)
+      .subscribe((response: any) => {
+        this.favorites = response;
+        console.log(response);
+      });
+    console.log(this.filteredRecipes);
+    this.favorites = this.filteredRecipes.filter((item): any => {
+      for (let favorite of this.favorites) {
+        return favorite.recipe.label === item.recipe.label;
+      }
+    });
+  };
 }
